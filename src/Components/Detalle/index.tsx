11670825c5473fa13ff8c73f653db8f2c960c7f8
Josue Patricio <josretamozovar@gmail.com>
2024-11-13 21:18:07 -0500

    trabajando con las cards de shadcn

--- src/Components/Detalle/index.tsx
+++ src/Components/Detalle/index.tsx
@@ -1,45 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
-
-interface CardProps {
-  url: string;
-  texto: string;
-}
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
 
 interface DetalleProps {
-  id: string;
+  id?: string;
   download_url: string;
   author: string;
 }
 
-const Card = ({ url = "", texto = "" }: CardProps) => {
+function DetalleCard({ id, download_url, author }: DetalleProps) {
   return (
-    <div className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 dark:bg-gray-800">
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2">
-          <img
-            alt="Plato"
-            src={url}
-            className="w-full h-64 md:h-full object-cover"
-          />
-        </div>
-        <div className="p-6 md:w-1/2 flex flex-col justify-center">
-          <h3 className="text-xl md:text-2xl font-semibold text-gray-800 dark:text-white mb-2">
-            {texto}
-          </h3>
-          <p className="text-gray-600 dark:text-gray-300 mb-4">
-            Descubre esta deliciosa opción preparada con los mejores
-            ingredientes.
-          </p>
-          <button className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition-colors mt-auto self-start">
-            Ver más
-          </button>
-        </div>
-      </div>
-    </div>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>{author}</CardTitle>
+        <CardDescription>{id}</CardDescription>
+      </CardHeader>
+      <CardContent>
+      <img src={download_url} alt={author} className="object-cover  *:h-48 w-96" />
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="register">Ver mas</Button>
+      </CardFooter>
+    </Card>
   );
-};
-
-
+}
 
 const Detalle = () => {
   const { isPending, error, data } = useQuery({
@@ -54,9 +45,9 @@ const Detalle = () => {
   return (
     <section className="py-12 bg-gray-50 dark:bg-gray-900">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="grid gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {data.map(({id,download_url,author}:DetalleProps) => (
-            <Card key={id} url={download_url} texto={author} />
+            <DetalleCard key={id} download_url={download_url} author={author} />
           ))}
         </div>
       </div>
